07de698712abe017106c81494b704d548520566f
Yazid Grim <mohamed.yazid.grim@avanade.com>
2021-12-23 18:13:38 -0500

    basic map and coloring

--- render.js
+++ render.js
@@ -1,22 +1,36 @@
-var w = 960;
-var h = 500;
+var w = 1500;
+var h = 1000;
+
+var backgroundColor = "#252525";
+var borderColor = "#636363"
 
 var projection = d3.geo.orthographic()
-                    .clipAngle(90)
+                    .clipAngle(180)
                     .rotate([98, -60])
-                    .scale(600)
+                    .scale(1200)
                     .translate([w/2, h/2]);
 
 var path = d3.geo.path().projection(projection);   
 
 var svg = d3.select("body").append("svg")
-                .attr({width: w, height: h});
+                .attr({
+                        width: w, 
+                        height: h
+                    });
 
-d3.json("https://raw.githubusercontent.com/serde-rs/json-benchmark/master/data/canada.json", function(json) {
+var background = svg.append("rect")
+                        .attr({
+                            width: "100%",
+                            height: "100%",
+                            fill: backgroundColor
+                        })
+
+d3.json("georef-canada-province.geojson", function(json) {
     svg.selectAll("path")
         .data(json.features)
         .enter()
         .append("path")
         .attr("d", path)
-        .attr("fill", "#666666");
+        .attr("fill", backgroundColor)
+        .attr("stroke", borderColor);
 });
